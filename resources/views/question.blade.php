55082118cf4e9f41a2237501d5da9c78314233d2
AlexDukachev <alex.dukachev@gmail.com>
2020-05-16 16:45:33 +0600

    1

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -31,7 +31,7 @@
                     </figure>
                 </div>
                 <div class="q-element">
-                    <div class="q-box" style="background-color:#ffffff; " data-hover="#0fbaf4;">
+                    <div class="q-box" style="background-color:#ffffff; " data-hover="0fbaf4">
                         <div class="content">
                             <h3 class="title">Creative activities</h3>
                             <div class="description" style="margin:0 0 13px; line-height: 1.5;">Help developing your child's creativity and social skills.</div>
@@ -39,7 +39,7 @@
                     </div>
                 </div>
                 <div class="q-element">
-                    <div class="q-box" style="background-color:#ffffff; " data-hover="#f96a9a;">
+                    <div class="q-box" style="background-color:#ffffff; " data-hover="f96a9a">
                         <div class="content">
                             <h3 class="title">Creative activities</h3>
                             <div class="description" style="margin:0 0 13px; line-height: 1.5;">Help developing your child's creativity and social skills.</div>
@@ -47,7 +47,7 @@
                     </div>
                 </div>
                 <div class="q-element">
-                    <div class="q-box" style="background-color:#ffffff; " data-hover="#f9b001;">
+                    <div class="q-box" style="background-color:#ffffff; " data-hover="f9b001">
                         <div class="content">
                             <h3 class="title">Creative activities</h3>
                             <div class="description" style="margin:0 0 13px; line-height: 1.5;">Help developing your child's creativity and social skills.</div>
@@ -55,7 +55,7 @@
                     </div>
                 </div>
                 <div class="q-element">
-                    <div class="q-box" style="background-color:#ffffff; " data-hover="#43cd66;">
+                    <div class="q-box" style="background-color:#ffffff; " data-hover="43cd66">
                         <div class="content">
                             <h3 class="title">Creative activities</h3>
                             <div class="description" style="margin:0 0 13px; line-height: 1.5;">Help developing your child's creativity and social skills.</div>
@@ -71,10 +71,9 @@
     <script>
         var scene = document.getElementById('scene');
         var parallax = new Parallax(scene);
-        $('.q-element')
-        .mouseenter(function() {
+        $('.q-element').mouseenter(function() {
             var color = $('.q-box', this).data('hover')
-            $('.q-box', this).css('background-color', color)
+            $('.q-box', this).css('background-color', '#' + color)
         })
         .mouseleave(function() {
             $('.q-box', this).css('background-color', '#FFFFFF')
