86d44d654bfe820cfe0bac6f6f2f2686fc708452
AlexDukachev <alex.dukachev@gmail.com>
2020-05-16 13:45:12 +0600

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Child Game</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('build/css/main.css') }}">
 </head>
 <body>
     
+    <header>
+        <div class="menu-shrink">
+            <div class="container">
+                <nav class="navbar navbar-expand-md navbar-light">
+                    <div class="logo">
+                        <a class="navbar-brand" href="{{ url('/') }}">Child Game</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
 </body>
 </html>
