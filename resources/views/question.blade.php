1197b48daab5ea327de0278895449a8641e71e7e
AlexDukachev <alex.dukachev@gmail.com>
2020-05-16 15:33:01 +0600

    1

---
+++ resources/views/question.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Child Game</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('build/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/css/main.css') }}">
+</head>
+<body>
+    
+    
+
+</body>
+</html>
